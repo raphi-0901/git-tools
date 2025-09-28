f6e767512f0c47333bb9cf446c01e8e3c9d15092
Raphael Wirnsberger <rwirnsberger@anexia-it.com>
2025-09-28 16:38:55 +0200

    feat: update auto-branch config to support object keys for api key and email

--- src/commands/auto-branch/config.ts
+++ src/commands/auto-branch/config.ts
@@ -1,9 +1,20 @@
-import { BaseConfigCommand } from "../../shared/base-config-command.js";
+import {BaseConfigCommand} from "../../shared/base-config-command.js";
 
 export default class AutoBranchConfigCommand extends BaseConfigCommand {
     constructor(argv: string[], config: never) {
         super(argv, config, {
-            allowedKeys: ["API_KEY", "GROQ_API_KEY", "INSTRUCTIONS", "EMAIL"],
+            allowedKeys: [
+                {
+                    isObject: true,
+                    key: "API_KEY",
+                },{
+                    isObject: true,
+                    key: "EMAIL",
+                },
+                "GROQ_API_KEY",
+                "INSTRUCTIONS",
+                "DEFAULT_HOSTNAME"
+            ],
             commandId: 'auto-branch',
         });
     }
--- src/types/auto-branch-config.ts
+++ src/types/auto-branch-config.ts
@@ -1,6 +1,7 @@
 export type AutoBranchConfig = {
-    API_KEY: string;
-    EMAIL: string;
+    API_KEY: Record<string, string>;
+    DEFAULT_HOSTNAME: string;
+    EMAIL: Record<string, string>;
     GROQ_API_KEY: string;
     INSTRUCTIONS: string;
 }
