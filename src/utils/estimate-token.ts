381a9def97675ed9bc5f990a030dd58ed6a9c757
Raphael Wirnsberger <rwirnsberger@anexia-it.com>
2025-11-05 19:02:55 +0100

    chore: delete estimate-token utility

--- src/utils/estimate-token.ts
+++
@@ -1,8 +0,0 @@
-import { encoding_for_model as encodingForModel } from "tiktoken";
-
-export async function estimateTokens(input: string) {
-    const enc = encodingForModel("gpt-4o-mini");
-    const tokens = enc.encode(input).length;
-    enc.free();
-    return tokens;
-}
