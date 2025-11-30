777a9c04e72dea33be4fc924b6dd6d5771c413bb
Raphael Wirnsberger <rwirnsberger@anexia-it.com>
2025-11-30 16:27:15 +0100

    feat: add select input component with cancel support

--- src/ui/SelectInput.tsx
+++ src/ui/SelectInput.tsx
@@ -2,22 +2,36 @@ import { Text, useInput } from "ink";
 import SelectInput from "ink-select-input";
 import React, { useEffect, useState } from "react";
 
+import { renderAnsweredQuestion } from "./helper/renderAnsweredQuestion.js";
+import { renderCancelledQuestion } from "./helper/renderCancelledQuestion.js";
+import { renderQuestion } from "./helper/renderQuestion.js";
 import { renderInkComponent } from "./renderInkComponent.js";
 
-export type SelectInputItem<T> = { label: string; value: T };
+export type SelectInputItem<T> = {
+    key?: string;
+    label: string;
+    value: T;
+};
 
 function SelectInputWithCancel<T>({
                                       cancel,
                                       items,
-                                      submit
+                                      message,
+                                        submit
                                   }: {
     cancel: () => void;
     items: SelectInputItem<T>[];
+    message: string;
     submit: (value: T) => void;
 }) {
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [isCancelling, setIsCancelling] = useState(false);
-    const [value, setValue] = useState<null | T>(null);
+    const [value, setValue] = useState<null | SelectInputItem<T>>(null);
+    const mappedItems = items.map(item => ({
+        key: item.key || item.label,
+        label: item.label,
+        value: item.value
+    }));
 
     useInput((input, key) => {
         if (key.ctrl && input === "c") {
@@ -26,7 +40,7 @@ function SelectInputWithCancel<T>({
     });
 
     const handleSelect = (item: SelectInputItem<T>) => {
-        setValue(item.value);
+        setValue(item);
         setIsSubmitting(true);
     };
 
@@ -36,34 +50,44 @@ function SelectInputWithCancel<T>({
             return;
         }
 
-        if (!isSubmitting) {
+        if (!isSubmitting || !value) {
             return;
         }
 
-        submit(value!);
+        submit(value.value);
     }, [isSubmitting, isCancelling]);
 
     if (isCancelling) {
-        return <Text>Cancelling…</Text>;
+        return renderCancelledQuestion(message)
     }
 
     if (isSubmitting) {
-        return <Text>Submitting…</Text>;
+        return renderAnsweredQuestion(message, value?.label || "")
     }
 
     return (
-        <SelectInput
-            items={items}
-            onSelect={handleSelect}
-        />
+        <>
+            {renderQuestion(message)}
+            <SelectInput
+                items={mappedItems}
+                onSelect={handleSelect}
+            />
+        </>
     );
 }
 
-export function renderSelectInput<T>(items: SelectInputItem<T>[]) {
+export function renderSelectInput<T>({
+    items,
+    message,
+                                     }: {
+    items: SelectInputItem<T>[],
+    message: string
+}) {
     return renderInkComponent<T>(({ cancel, submit }) => (
         <SelectInputWithCancel
             cancel={cancel}
             items={items}
+            message={message}
             submit={submit}
         />
     ));
