8805a9a4621855a2496af3244898122468563aa8
Raphael Wirnsberger <rwirnsberger@anexia-it.com>
2025-12-09 11:49:06 +0100

    refactor: use --cached flag in checkIfFilesStaged

--- src/utils/check-if-files-staged.ts
+++ src/utils/check-if-files-staged.ts
@@ -2,7 +2,7 @@ import { simpleGit } from "simple-git";
 
 export async function checkIfFilesStaged() {
     const git = simpleGit();
-    const diff = await git.diff();
+    const diff = await git.diff([ '--cached']);
 
     return diff.trim().length > 0;
 }
