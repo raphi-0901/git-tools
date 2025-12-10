d2a6d656b17d073562cd59b9d7ad36a3f5497717
Raphael Wirnsberger <rwirnsberger@anexia-it.com>
2025-12-10 17:17:19 +0100

    feat: add base command abstract class

---
+++ src/base-commands/BaseCommand.ts
@@ -0,0 +1,24 @@
+import { Command, Errors } from "@oclif/core";
+import chalk from "chalk";
+
+import { FATAL_ERROR_NUMBER } from "../utils/constants.js";
+import * as LOGGER from "../utils/logging.js";
+import { createSpinner } from "../utils/spinner.js";
+
+export abstract class BaseCommand extends Command {
+    public abstract readonly commandId: string;
+    public abstract readonly configId: string;
+
+    public readonly spinner = createSpinner();
+
+    async catch(error: unknown) {
+        // skip errors already logged by LOGGER.fatal
+        if(error instanceof Errors.ExitError && error.oclif.exit === FATAL_ERROR_NUMBER) {
+            return;
+        }
+
+        LOGGER.error(this, error as string)
+    }
+
+    public abstract run(): Promise<void>;
+}
