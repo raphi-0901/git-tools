20bb40a80ddb2f0af637d6192228b2da44293d27
Raphael Wirnsberger <rwirnsberger@anexia-it.com>
2025-12-26 20:21:18 +0100

    refactor: update ESLint configuration to enforce stricter TypeScript rules and improve code style consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,16 +4,79 @@ import prettier from 'eslint-config-prettier'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-const gitignorePath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '.gitignore')
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const gitignorePath = path.resolve(__dirname, '.gitignore')
 
-export default [includeIgnoreFile(gitignorePath),
-    ...oclif,
-    prettier,
-    {
-        rules: {
-            "no-await-in-loop": "off",
-            "object-curly-spacing": ["error", "always"],
-            'unicorn/filename-case': 'off',
+export default [
+  includeIgnoreFile(gitignorePath),
+  ...oclif,
+  prettier,
+  {
+    files: ['*.ts', '*.tsx'],
+    rules: {
+      '@typescript-eslint/consistent-type-assertions': 'error',
+      '@typescript-eslint/member-delimiter-style': ['error', {
+        multiline: {
+          delimiter: 'none',
+          requireLast: false,
         },
-    }
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
+      }],
+      '@typescript-eslint/no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+      }],
+      '@typescript-eslint/strict-boolean-expressions': 'warn',
+    },
+  },
+
+  {
+    files: ['**/*'],
+    rules: {
+      '@stylistic/comma-dangle': ['error', 'only-multiline'],
+      '@stylistic/comma-spacing': ['error', {
+        'after': true,
+        'before': false,
+      }],
+      '@stylistic/key-spacing': ['error', {
+        'beforeColon': false,
+      }],
+      '@stylistic/object-curly-newline': ['error', {
+        'ExportDeclaration': {
+          'minProperties': 3,
+          'multiline': true,
+        },
+        'ImportDeclaration': 'never',
+        'ObjectExpression': 'always',
+        'ObjectPattern': {
+          'minProperties': 3,
+          'multiline': true,
+        },
+      }],
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+      '@stylistic/object-property-newline': 'error',
+      'arrow-body-style': ['error', 'as-needed'],
+      'brace-style': ['error', '1tbs', {
+        allowSingleLine: false,
+      }],
+      'curly': ['error', 'all'],
+      'max-len': ['warn', {
+        code: 150,
+      }],
+      'no-await-in-loop': 'off',
+      'no-console': 'warn',
+      'no-nested-ternary': 'error',
+      'prefer-const': ['error', {
+        destructuring: 'all',
+        ignoreReadBeforeAssign: true,
+      }],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'unicorn/filename-case': 'off',
+      'unicorn/no-array-reduce': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+    },
+  },
 ]
